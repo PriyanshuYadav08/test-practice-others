44990b7c06faa6037890090c9ed114095436e0f6
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-02-18 12:39:21 +0530

    q2_update

--- DSC 10/Assignment 2/question2_ass2.R
+++ DSC 10/Assignment 2/question2_ass2.R
@@ -3,11 +3,10 @@ x <- seq(0,20,by = 1)
 png(file = "dpois.png") 
 plot(x,y) 
 dev.off()
+print(x <- ppois(16, lambda=12))
 
-ppois(16, lambda=12)
+print(x <- ppois(16, lambda=12, lower=FALSE))
 
-ppois(16, lambda=12, lower=FALSE)
+print(x <- qpois(0.75, lambda=12))
 
-qpois(0.75, lambda=12)
-
-rpois(16, lambda=12)
+print(x <- rpois(16, lambda=12))

da2f9296f46af72696f50fd3323c9bce1d4ca311
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-02-28 11:28:45 +0530

    mergesort

--- DSC 11 Practicals/1_mergesort.cpp
+++ DSC 11 Practicals/1_mergesort.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+
 int main()
 {
-    
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
 }

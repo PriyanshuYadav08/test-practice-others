31bb077ce81fc32be9b2f0b86977fa6e6b568658
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-02-28 11:55:26 +0530

    insertionsort

--- DSC 11 Practicals/1_mergesort.cpp
+++ DSC 11 Practicals/1_mergesort.cpp
@@ -1,17 +1,15 @@
+//MERGE SORT
+
 #include <bits/stdc++.h>
 using namespace std;
-
 void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
     int L[n1], R[n2];
-
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[mid + 1 + j];
-
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
@@ -23,13 +21,11 @@ void merge(int arr[], int left, int mid, int right) {
         }
         k++;
     }
-
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -40,7 +36,6 @@ void merge(int arr[], int left, int mid, int right) {
 void mergeSort(int arr[], int left, int right) {
     if (left >= right)
         return;
-
     int mid = left + (right - left) / 2;
     mergeSort(arr, left, mid);
     mergeSort(arr, mid + 1, right);
@@ -49,16 +44,14 @@ void mergeSort(int arr[], int left, int right) {
 
 int main() {
     int n;
-    cout << "Enter the number of elements: ";
+    cout << "Enter the number of elements = ";
     cin >> n;
     int arr[n];
-    cout << "Enter the elements: ";
+    cout << "Enter the elements = ";
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-
     mergeSort(arr, 0, n - 1);
-
-    cout << "Sorted array: ";
+    cout << "Sorted array = ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
